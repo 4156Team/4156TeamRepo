3ee2f899dc42138cfddfce6297efb5137402e360
yumiaoli <yl4225@columbia.edu>
2020-11-08 16:42:03 +0800

    Add register function and register web page

--- src/main/java/com/java/rollercoaster/service/UserService.java
+++ src/main/java/com/java/rollercoaster/service/UserService.java
@@ -1,8 +1,10 @@
 package com.java.rollercoaster.service;
 
+import com.java.rollercoaster.errorEnum.BusinessException;
 import com.java.rollercoaster.service.model.UserModel;
 
 public interface UserService {
 //    UserModel getUserByPhoneNumber(String phoneNumber);
     UserModel getUserByUserId(Integer userId);
+    void register(UserModel userModel) throws BusinessException;
 }
