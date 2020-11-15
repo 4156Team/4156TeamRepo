85e6db5c8afd7490af560474306212f632a7ebd2
JamesYuanYuzhang <jamesyuanyuzhang@gmail.com>
2020-11-15 12:20:20 +0800

    test create junit test5

---
+++ src/test/java/com/java/rollercoaster/service/impl/AppointmentServiceImplTest.java
@@ -0,0 +1,20 @@
+package com.java.rollercoaster.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppointmentServiceImplTest {
+
+    @Test
+    void addAppointment() {
+    }
+
+    @Test
+    void updateAppointment() {
+    }
+
+    @Test
+    void deleteAppointment() {
+    }
+}
