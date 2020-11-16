2980ae1dcf995589083af65544af73f950de7b7b
yumiaoli <yl4225@columbia.edu>
2020-11-16 15:38:53 +0800

    fix register and login html

--- src/main/java/com/java/rollercoaster/controller/BaseController.java
+++ src/main/java/com/java/rollercoaster/controller/BaseController.java
@@ -23,21 +23,21 @@ public class BaseController {
      * @param ex exception
      * @return a response with defined common type
      */
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.OK)
-    @ResponseBody
-    public Object handlerException(HttpServletRequest request, Exception ex) {
-        Map<String, Object> responseData = new HashMap<>();
-        if (ex instanceof BusinessException) {
-            BusinessException businessException = (BusinessException) ex;
-            responseData.put("errCode", businessException.getErrCode());
-            responseData.put("errMsg", businessException.getErrMsg());
-        } else {
-            responseData.put("errCode", ErrorEnum.UNKNOWN_ERROR.getErrCode());
-            responseData.put("errMsg", ErrorEnum.UNKNOWN_ERROR.getErrMsg());
-        }
-        return CommonReturnType.create(responseData,"fail");
-
-    }
+//    @ExceptionHandler(Exception.class)
+//    @ResponseStatus(HttpStatus.OK)
+//    @ResponseBody
+//    public Object handlerException(HttpServletRequest request, Exception ex) {
+//        Map<String, Object> responseData = new HashMap<>();
+//        if (ex instanceof BusinessException) {
+//            BusinessException businessException = (BusinessException) ex;
+//            responseData.put("errCode", businessException.getErrCode());
+//            responseData.put("errMsg", businessException.getErrMsg());
+//        } else {
+//            responseData.put("errCode", ErrorEnum.UNKNOWN_ERROR.getErrCode());
+//            responseData.put("errMsg", ErrorEnum.UNKNOWN_ERROR.getErrMsg());
+//        }
+//        return CommonReturnType.create(responseData,"fail");
+//
+//    }
 
 }
