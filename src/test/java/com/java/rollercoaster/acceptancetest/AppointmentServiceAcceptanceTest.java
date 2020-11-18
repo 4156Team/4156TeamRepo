0fb0ab9e4f6f04a984afd383db060b866b085abd
yuanmxia <yx2548@columbia.edu>
2020-11-17 22:04:59 -0500

    appoint acceptence test

--- src/test/java/com/java/rollercoaster/acceptancetest/AppointmentServiceAcceptanceTest.java
+++ src/test/java/com/java/rollercoaster/acceptancetest/AppointmentServiceAcceptanceTest.java
@@ -188,7 +188,7 @@ public class AppointmentServiceAcceptanceTest {
     }
 
     @Test
-    public void cancelAppointmentsTest() throws JsonProcessingException {
+    public void cancelAppointmentsTest() {
         //register
         String url = "http://localhost:8080/user/register";
         MultiValueMap<String, Object> paramMap = new LinkedMultiValueMap<String, Object>();
@@ -250,6 +250,78 @@ public class AppointmentServiceAcceptanceTest {
         userAccountMapper.deleteByExample(userAccountExample);
         userPasswordMapper.deleteByPrimaryKey(userId);
         eventMapper.deleteByPrimaryKey("event test");
+    }
+
+    @Test
+    public void updateAppointmentsTest() {
+        //register
+        String url = "http://localhost:8080/user/register";
+        MultiValueMap<String, Object> paramMap = new LinkedMultiValueMap<String, Object>();
+        paramMap.add("telephone", "6789");
+        paramMap.add("name", "James");
+        paramMap.add("gender", "male");
+        paramMap.add("age", 18);
+        paramMap.add("password", "6789");
+        restTemplate.postForObject(url, paramMap, CommonReturnType.class);
+
+        //log in
+        MultiValueMap<String, Object> paramMap1 = new LinkedMultiValueMap<String, Object>();
+        String url1 = "http://localhost:8080/user/login";
+        paramMap1.add("telephone", "6789");
+        paramMap1.add("password", "6789");
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<MultiValueMap<String, Object>> httpEntity =
+                new HttpEntity<MultiValueMap<String, Object>>(paramMap1, headers);
+        //restTemplate.postForObject(url1, paramMap1, CommonReturnType.class);
+        ResponseEntity<CommonReturnType> responseEntity = restTemplate.postForEntity(url1, httpEntity, CommonReturnType.class);
+        String cookie = getCookie(responseEntity);
+
+
+        //insert event test to the table
+        Event event = initEvent(10, "event test");
+        Event anotherEvent = initEvent(5, "event another");
+        //add appointments
+        Appointment appointment = new Appointment();
+        appointment.setEventName("event test");
+        String url2 = "http://localhost:8080/appointment/addAppointment";
+        HttpHeaders headers1 = new HttpHeaders();
+        headers1.add("Cookie",cookie );
+        HttpEntity<Appointment> httpEntity1 = new HttpEntity<>(appointment, headers1);
+        ResponseEntity<CommonReturnType> response = restTemplate.postForEntity(url2, httpEntity1, CommonReturnType.class);
+        assertEquals("success", response.getBody().getStatus());
+
+        UserAccountExample userAccountExample = new UserAccountExample();
+        UserAccountExample.Criteria userAccountExampleCriteria = userAccountExample.createCriteria();
+        userAccountExampleCriteria.andPhoneNumberEqualTo("6789");
+        List<UserAccount> userAccount = userAccountMapper.selectByExample(userAccountExample);
+        int userId = userAccount.get(0).getUserId();
+
+        AppointmentExample appointmentExample = new AppointmentExample();
+        AppointmentExample.Criteria appointmentCriteria = appointmentExample.createCriteria();
+        appointmentCriteria.andUserIdEqualTo(userAccount.get(0).getUserId());
+        List<Appointment> appointmentsList = appointmentMapper.selectByExample(appointmentExample);
+
+        Appointment appointment1 = appointmentsList.get(0);
+        appointment1.setEventName("event another");
+        //update appointments
+        String url3 = "http://localhost:8080/appointment/updateAppointment";
+
+        MultiValueMap<String, Object> paramMap2 = new LinkedMultiValueMap<String, Object>();
+        paramMap2.add("appointmentId", appointment1.getAppointmentid());
+        HttpHeaders headers2 = new HttpHeaders();
+        headers2.add("Cookie",cookie );
+        HttpEntity<Appointment> httpEntity2 = new HttpEntity<>(appointment1, headers2);
+        response = restTemplate.postForEntity(url3,httpEntity2,CommonReturnType.class);
+        assertEquals("event another",
+                appointmentMapper.selectByPrimaryKey(appointment1.getAppointmentid()).getEventName());
+        assertEquals(userId, appointmentMapper.selectByPrimaryKey(appointment1.getAppointmentid()).getUserId());
+
+        //delete record
+        userAccountMapper.deleteByExample(userAccountExample);
+        userPasswordMapper.deleteByPrimaryKey(userId);
+        eventMapper.deleteByPrimaryKey("event test");
+        eventMapper.deleteByPrimaryKey("event another");
+        appointmentMapper.deleteByPrimaryKey(appointment1.getAppointmentid());
     }
 
 }
