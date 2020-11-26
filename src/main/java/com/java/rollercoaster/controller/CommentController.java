666946b71aa9aaa4497d8f9a62f22ff49559f2f8
yumiaoli <yl4225@columbia.edu>
2020-11-26 20:02:29 +0800

    comment controller and controller test

---
+++ src/main/java/com/java/rollercoaster/controller/CommentController.java
@@ -0,0 +1,63 @@
+package com.java.rollercoaster.controller;
+
+import com.java.rollercoaster.errorenum.ErrorEnum;
+import com.java.rollercoaster.pojo.Ticket;
+import com.java.rollercoaster.response.CommonReturnType;
+import com.java.rollercoaster.service.CommentService;
+import com.java.rollercoaster.service.model.CommentModel;
+import com.java.rollercoaster.service.model.UserModel;
+import com.java.rollercoaster.service.model.enumeration.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller("comment")
+@RequestMapping("/comment")
+@CrossOrigin(allowCredentials = "true", allowedHeaders = "*")
+public class CommentController {
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+    @Autowired
+    private CommentService commentService;
+
+
+    @PostMapping("/postComment")
+    @ResponseBody
+    public CommonReturnType postComment(@RequestBody CommentModel commentModel) {
+        Boolean isLogin = (Boolean) httpServletRequest.getSession().getAttribute("IS_LOGIN");
+        if (isLogin == null || !isLogin)  {
+            return CommonReturnType.autoCreate(ErrorEnum.USER_NOT_LOGIN);
+        }
+        UserModel userModel = (UserModel) httpServletRequest
+                .getSession().getAttribute("LOGIN_USER");
+        //if user not exist
+        if (userModel == null) {
+            return CommonReturnType.autoCreate(ErrorEnum.USER_NOT_EXIST);
+        }
+        return CommonReturnType.autoCreate(commentService.addComment(userModel, commentModel));
+    }
+
+    @PostMapping("/deleteComment")
+    @ResponseBody
+    public CommonReturnType deleteComment(@RequestBody CommentModel commentModel) {
+        Boolean isLogin = (Boolean) httpServletRequest.getSession().getAttribute("IS_LOGIN");
+        if (isLogin == null || !isLogin)  {
+            return CommonReturnType.autoCreate(ErrorEnum.USER_NOT_LOGIN);
+        }
+        UserModel userModel = (UserModel) httpServletRequest
+                .getSession().getAttribute("LOGIN_USER");
+        //if user not exist
+        if (userModel == null) {
+            return CommonReturnType.autoCreate(ErrorEnum.USER_NOT_EXIST);
+        }
+        return CommonReturnType.autoCreate(commentService.deleteComment(userModel, commentModel));
+    }
+
+
+}
