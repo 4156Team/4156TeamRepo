16b17c5ef5dfd58853f02592b2e80e32805e1e70
yumiaoli <yl4225@columbia.edu>
2020-12-05 11:10:09 +0800

    comment the mail service

--- src/main/java/com/java/rollercoaster/service/impl/MailServiceImpl.java
+++ src/main/java/com/java/rollercoaster/service/impl/MailServiceImpl.java
@@ -16,32 +16,32 @@ public class MailServiceImpl implements MailService {
     @Override
     public  ErrorEnum sendAnnouncementMessage(String toEmail, String text)
             throws BusinessException, UnirestException {
-        Unirest.post(
-                "https://api.mailgun.net/v3/sandbox577c98fd40d242c3bf95fbedf216dd8a.mailgun.org/messages")
-                .basicAuth("api", "2362f0e8ad721fcfe14b2a6507ef27a6-95f6ca46-cee0c017")
-                .queryString("from",
-                        "Roller Coaster <mailgun@sandbox577c98fd40d242c3bf95fbedf216dd8a"
-                                + ".mailgun.org>")
-                .queryString("to", toEmail)
-                .queryString("subject", "New announcement from Roller coaster")
-                .queryString("text", text)
-                .asJson();
+//        Unirest.post(
+//                "https://api.mailgun.net/v3/sandbox577c98fd40d242c3bf95fbedf216dd8a.mailgun.org/messages")
+//                .basicAuth("api", "2362f0e8ad721fcfe14b2a6507ef27a6-95f6ca46-cee0c017")
+//                .queryString("from",
+//                        "Roller Coaster <mailgun@sandbox577c98fd40d242c3bf95fbedf216dd8a"
+//                                + ".mailgun.org>")
+//                .queryString("to", toEmail)
+//                .queryString("subject", "New announcement from Roller coaster")
+//                .queryString("text", text)
+//                .asJson();
         return ErrorEnum.OK;
 
     }
 
     @Override
     public ErrorEnum sendTicketMessage(String toEmail, String text) throws UnirestException {
-        HttpResponse<JsonNode> request = Unirest.post(
-                "https://api.mailgun.net/v3/sandbox577c98fd40d242c3bf95fbedf216dd8a.mailgun.org/messages")
-                .basicAuth("api", "2362f0e8ad721fcfe14b2a6507ef27a6-95f6ca46-cee0c017")
-                .queryString("from",
-                        "Roller Coaster <mailgun@sandbox577c98fd40d242c3bf95fbedf216dd8a"
-                        + ".mailgun.org>")
-                .queryString("to", toEmail)
-                .queryString("subject", "Your ticket of Roller coaster!")
-                .queryString("text", text)
-                .asJson();
+//        HttpResponse<JsonNode> request = Unirest.post(
+//                "https://api.mailgun.net/v3/sandbox577c98fd40d242c3bf95fbedf216dd8a.mailgun.org/messages")
+//                .basicAuth("api", "2362f0e8ad721fcfe14b2a6507ef27a6-95f6ca46-cee0c017")
+//                .queryString("from",
+//                        "Roller Coaster <mailgun@sandbox577c98fd40d242c3bf95fbedf216dd8a"
+//                        + ".mailgun.org>")
+//                .queryString("to", toEmail)
+//                .queryString("subject", "Your ticket of Roller coaster!")
+//                .queryString("text", text)
+//                .asJson();
 
         return ErrorEnum.OK;
 
